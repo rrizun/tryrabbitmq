84f80fd862b28187f97d0c5ae71d55366d5e1888
rrizun <rrizun>
2013-01-28 16:08:37 -0800

    checkpoint

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <event2/event.h>
+//#include <event2/event.h>
 
 // C++ includes
 #include <map>
@@ -28,6 +28,9 @@
 using namespace std;
 using namespace boost;
 using namespace myrss;
+using namespace google::protobuf;
+
+#define SUMMARY_EVERY_US 1000000
 
 void die_on_error(int x, char const *context) {
   if (x < 0) {
@@ -119,6 +122,15 @@ struct auto_amqp_connection_state_t {
 //}
 
 template<class T>
+class CmdHandler {
+public:
+	// dtor
+	virtual ~CmdHandler(){}
+	// override
+	virtual void handleCmd(T *cmd)=0;
+};
+
+template<class T>
 class EventHandler {
 public:
 	// dtor
@@ -127,15 +139,15 @@ public:
 	virtual void handleEvent(T *event)=0;
 };
 
+class Dispatcher {
+public:
+	virtual ~Dispatcher(){}
+	virtual void dispatch(Message *raw) = 0;
+};
+
+typedef map< string, shared_ptr<Dispatcher> > DispatcherMap;
+
 class EventBus {
-
-	event_base *base;
-
-	class Dispatcher {
-	public:
-		virtual ~Dispatcher(){}
-		virtual void dispatch(string raw) = 0;
-	};
 
 	template<class T>
 	class Dispatcher0: public Dispatcher {
@@ -144,13 +156,20 @@ class EventBus {
 		// ctor
 		Dispatcher0(shared_ptr< EventHandler<T> > handler): handler(handler) {
 		}
-		virtual void dispatch(string raw) {
+		virtual void dispatch(Message *raw) {
+
+//			OWConfig aaa;
+//			aaa.CopyFrom(*raw);
+
 			T event;
+			event.CopyFrom(*raw);
+//			event.set_value("qqq");
 			// fill
 			handler->handleEvent(&event);
 		}
 	};
-	map< string, shared_ptr<Dispatcher> > dispatchers;
+public:
+	DispatcherMap dispatchers;
 //	void handleEvent(Message *message) {
 //		string name (message->GetDescriptor()->name())
 //
@@ -158,8 +177,7 @@ class EventBus {
 //	}
 public:
 	// ctor
-	EventBus(event_base *base): base(base) {
-
+	EventBus() {
 	}
 
 	~EventBus() {
@@ -168,76 +186,218 @@ public:
 //		  die_on_error(amqp_destroy_connection(conn), "Ending connection");
 	}
 
-	// client api
-	template<class CmdMsg>
-	void postCmd(CmdMsg *cmdMsg) {
-		string name(cmdMsg->GetDescriptor()->name().c_str());
-		printf("postCmd: name=%s\n", name.c_str());
-		dispatchers[name]->dispatch("zzz");
-	}
+//	// client api
+//	template<class CmdMsg>
+//	void postCmd(CmdMsg *cmdMsg) {
+//		string name(cmdMsg->GetDescriptor()->name().c_str());
+//		printf("postCmd: name=%s\n", name.c_str());
+//		dispatchers[name]->dispatch("zzz");
+//	}
+//	template<class EventMsg>
+//	void postEvent(EventMsg *eventMsg) {
+//		string name(eventMsg->GetDescriptor()->name().c_str());
+//		printf("postEventMsg: name=%s\n", name.c_str());
+//
+//		//		dispatchers[name]->dispatch("zzz");
+//
+//		  auto_amqp_connection_state_t state(amqp_new_connection()); // amqp_destroy_connection
+//
+//		  amqp_set_sockfd(state.get(), amqp_open_socket("localhost", 5672));
+//
+//		  die_on_amqp_error(amqp_login(state.get(), "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"), "Logging in");
+//		  amqp_channel_open(state.get(), 1);
+//		  die_on_amqp_error(amqp_get_rpc_reply(state.get()), "Opening channel");
+//
+//		  {
+//		    amqp_basic_properties_t props;
+//		    props._flags = AMQP_BASIC_CONTENT_TYPE_FLAG | AMQP_BASIC_DELIVERY_MODE_FLAG;
+//		    props.content_type = amqp_cstring_bytes("text/plain");
+//		    props.delivery_mode = 2; /* persistent delivery mode */
+//
+//		    string exchange(name);
+//		    string payload((*eventMsg).SerializeAsString());
+//		    string routingkey;
+//
+//		    die_on_error(amqp_basic_publish(state.get(),
+//						    1,
+//						    amqp_cstring_bytes(exchange.c_str()),
+//						    amqp_cstring_bytes(routingkey.c_str()),
+//						    0,
+//						    0,
+//						    &props,
+//						    amqp_cstring_bytes(payload.c_str())),
+//				 "Publishing");
+//		  }
+//
+//		  die_on_amqp_error(amqp_channel_close(state.get(), 1, AMQP_REPLY_SUCCESS), "Closing channel");
+//		  die_on_amqp_error(amqp_connection_close(state.get(), AMQP_REPLY_SUCCESS), "Closing connection");
+//	}
+
+//	// handler api
+//	template<class EventMsg>
+//	void addHandler(shared_ptr< EventHandler<EventMsg> > handler) {
+//		string name(EventMsg::descriptor()->name().c_str());
+//
+//		printf("addHandler: %s\n", EventMsg::descriptor()->name().c_str());
+//
+//		EventMsg event;
+//		event.PrintDebugString();
+//		dispatchers[name]=shared_ptr<Dispatcher>(new Dispatcher0<EventMsg>(handler));
+//	}
+//
 	template<class EventMsg>
-	void postEvent(EventMsg *eventMsg) {
-		string name(eventMsg->GetDescriptor()->name().c_str());
-		printf("postEventMsg: name=%s\n", name.c_str());
-
-		//		dispatchers[name]->dispatch("zzz");
-
-		  auto_amqp_connection_state_t state(amqp_new_connection()); // amqp_destroy_connection
-
-		  amqp_set_sockfd(state.get(), amqp_open_socket("localhost", 5672));
-
-		  die_on_amqp_error(amqp_login(state.get(), "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"), "Logging in");
-		  amqp_channel_open(state.get(), 1);
-		  die_on_amqp_error(amqp_get_rpc_reply(state.get()), "Opening channel");
-
-		  {
-		    amqp_basic_properties_t props;
-		    props._flags = AMQP_BASIC_CONTENT_TYPE_FLAG | AMQP_BASIC_DELIVERY_MODE_FLAG;
-		    props.content_type = amqp_cstring_bytes("text/plain");
-		    props.delivery_mode = 2; /* persistent delivery mode */
-
-		    string exchange(name);
-		    string payload((*eventMsg).SerializeAsString());
-		    string routingkey;
-
-		    die_on_error(amqp_basic_publish(state.get(),
-						    1,
-						    amqp_cstring_bytes(exchange.c_str()),
-						    amqp_cstring_bytes(routingkey.c_str()),
-						    0,
-						    0,
-						    &props,
-						    amqp_cstring_bytes(payload.c_str())),
-				 "Publishing");
-		  }
-
-		  die_on_amqp_error(amqp_channel_close(state.get(), 1, AMQP_REPLY_SUCCESS), "Closing channel");
-		  die_on_amqp_error(amqp_connection_close(state.get(), AMQP_REPLY_SUCCESS), "Closing connection");
-	}
-
-	// handler api
-	template<class EventMsg>
-	void addHandler(shared_ptr< EventHandler<EventMsg> > handler) {
+	void eventHandler(shared_ptr< EventHandler<EventMsg> > handler) {
 		string name(EventMsg::descriptor()->name().c_str());
-
-		printf("addHandler: %s\n", EventMsg::descriptor()->name().c_str());
-
-		EventMsg event;
-		event.PrintDebugString();
+		printf("eventHandler: %s\n", EventMsg::descriptor()->name().c_str());
 		dispatchers[name]=shared_ptr<Dispatcher>(new Dispatcher0<EventMsg>(handler));
 	}
+	void run(amqp_connection_state_t conn)
+	{
+	  uint64_t start_time = 0;
+	  int received = 0;
+	  int previous_received = 0;
+	  uint64_t previous_report_time = start_time;
+	  uint64_t next_summary_time = start_time + SUMMARY_EVERY_US;
 
-};
+	  amqp_frame_t frame;
+	  int result;
+	  size_t body_received;
+	  size_t body_target;
 
-class MyResetBladeEventHandler: public EventHandler<ResetBladeEvent> {
-public:
-	// override
-	virtual void handleEvent(ResetBladeEvent *event) {
-		printf("MyResetBladeHandler::handleEvent\n");
+	  uint64_t now;
+
+	  while (1) {
+	    now = 0;
+	    if (now > next_summary_time) {
+	      int countOverInterval = received - previous_received;
+	      double intervalRate = countOverInterval / ((now - previous_report_time) / 1000000.0);
+	      printf("%d ms: Received %d - %d since last report (%d Hz)\n",
+		     (int)(now - start_time) / 1000, received, countOverInterval, (int) intervalRate);
+
+	      previous_received = received;
+	      previous_report_time = now;
+	      next_summary_time += SUMMARY_EVERY_US;
+	    }
+
+	    amqp_maybe_release_buffers(conn);
+	    result = amqp_simple_wait_frame(conn, &frame);
+	    if (result < 0)
+	      return;
+
+	    if (frame.frame_type != AMQP_FRAME_METHOD)
+	      continue;
+
+	    if (frame.payload.method.id != AMQP_BASIC_DELIVER_METHOD)
+	      continue;
+
+	    amqp_basic_deliver_t *d;
+	    d = (amqp_basic_deliver_t *) frame.payload.method.decoded;
+	    printf("Delivery %u, exchange %.*s routingkey %.*s\n",
+		     (unsigned) d->delivery_tag,
+		     (int) d->exchange.len, (char *) d->exchange.bytes,
+		     (int) d->routing_key.len, (char *) d->routing_key.bytes);
+
+	    result = amqp_simple_wait_frame(conn, &frame);
+	    if (result < 0)
+	      return;
+
+	    if (frame.frame_type != AMQP_FRAME_HEADER) {
+	      fprintf(stderr, "Expected header!");
+	      abort();
+	    }
+
+	    body_target = frame.payload.properties.body_size;
+	    body_received = 0;
+
+	    string payload;
+	    while (body_received < body_target) {
+	      result = amqp_simple_wait_frame(conn, &frame);
+	      if (result < 0)
+		return;
+
+	      if (frame.frame_type != AMQP_FRAME_BODY) {
+		fprintf(stderr, "Expected body!");
+		abort();
+	      }
+
+	      body_received += frame.payload.body_fragment.len;
+	      string fragment;
+	      fragment.assign((const char *)frame.payload.body_fragment.bytes, frame.payload.body_fragment.len);
+	      payload+=fragment;
+	      assert(body_received <= body_target);
+	    }
+
+	    received++;
+
+	    printf("yeah!\n");
+	    OWConfig config;
+	    config.set_value(payload);
+	    dispatchers["OWConfig"]->dispatch(&config);
+	  }
+	}
+
+	int dispatch() {
+	//  char const *hostname;
+	//  int port;
+	//  char const *exchange;
+
+	  int sockfd;
+	  amqp_connection_state_t conn;
+
+	//  const char *exchange = "OWConfig"; /* argv[3]; */
+
+	  conn = amqp_new_connection();
+
+	  die_on_error(sockfd = amqp_open_socket("localhost", 5672), "Opening socket");
+	  amqp_set_sockfd(conn, sockfd);
+	  die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"),
+			    "Logging in");
+	  amqp_channel_open(conn, 1);
+	  die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
+
+	  for (DispatcherMap::iterator iter = dispatchers.begin(); iter != dispatchers.end(); ++iter) {
+		  amqp_bytes_t queuename;
+		  string exchange((*iter).first);
+		  {
+		    amqp_queue_declare_ok_t *r = amqp_queue_declare(conn, 1, amqp_empty_bytes, 0, 0, 0, 1,
+								    amqp_empty_table);
+		    die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue");
+		    queuename = amqp_bytes_malloc_dup(r->queue);
+		    if (queuename.bytes == NULL) {
+		      fprintf(stderr, "Out of memory while copying queue name");
+		      return 1;
+		    }
+		  }
+
+		  amqp_exchange_declare(conn, 1, amqp_cstring_bytes(exchange.c_str()), amqp_cstring_bytes("fanout"),
+					0, 0, amqp_empty_table);
+		  die_on_amqp_error(amqp_get_rpc_reply(conn), "amqp_exchange_declare");
+
+		  amqp_queue_bind(conn, 1, queuename, amqp_cstring_bytes(exchange.c_str()), amqp_cstring_bytes(""/*bindingkey*/),
+				  amqp_empty_table);
+		  die_on_amqp_error(amqp_get_rpc_reply(conn), "amqp_queue_bind");
+
+		  amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
+		  die_on_amqp_error(amqp_get_rpc_reply(conn), "amqp_basic_consume");
+	  }
+
+	  run(conn);
+
+	  die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
+	  die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
+	  die_on_error(amqp_destroy_connection(conn), "Ending connection");
+
+	  return 0;
 	}
 };
 
-static shared_ptr<event_base> base(event_base_new(), event_base_free);
+class MyEventHandler: public EventHandler<OWConfig> {
+public:
+	// override
+	virtual void handleEvent(OWConfig *event) {
+		printf("MyEventHandler::handleEvent value=%s\n", event->value().c_str());
+	}
+};
 
 int main(void) {
 //	time_t now(time(0));
@@ -245,18 +405,13 @@ int main(void) {
 
 	printf("amqp_version=%s\n", amqp_version());
 
-	EventBus eventBus(base.get());
+	EventBus bus;
 
-	// add handler
-	eventBus.addHandler(shared_ptr<EventHandler<ResetBladeEvent> >(new MyResetBladeEventHandler()));
+	bus.eventHandler(shared_ptr<EventHandler<OWConfig> >(new MyEventHandler()));
 
-	// post eventMsg
-	ResetBladeEvent eventMsg;
-	eventBus.postEvent(&eventMsg);
+	bus.dispatch();
 
-	sleep(1); // seconds
-
-	return event_base_dispatch(base.get());
+//	return sleep(86400); // seconds
 }
 
 //Thanks for reply Alan.
