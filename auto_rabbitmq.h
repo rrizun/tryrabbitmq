f64b9ba960a2f26743e53bb8dcd68125944a17c2
rrizun <rrizun>
2013-01-30 07:37:15 -0800

    use a single private queue for all events

--- auto_rabbitmq.h
+++ auto_rabbitmq.h
@@ -218,22 +218,23 @@ public:
 				amqp_channel_open(conn.get(), 1); // rpc
 				die_on_amqp_error(amqp_get_rpc_reply(conn.get()), "amqp_channel_open");
 
+				// create a private queue to receive events (one single queue for all events)
+				amqp_queue_declare_ok_t *queue_declare_ok = amqp_queue_declare(conn.get(), 1, amqp_empty_bytes, 0, 0, 0, 1, amqp_empty_table);
+				die_on_amqp_error(amqp_get_rpc_reply(conn.get()), "amqp_queue_declare");
+
+				auto_amqp_bytes privateQueue(amqp_bytes_malloc_dup(queue_declare_ok->queue));
+
+				amqp_basic_consume(conn.get(), 1, privateQueue.get(), amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
+				die_on_amqp_error(amqp_get_rpc_reply(conn.get()), "amqp_basic_consume");
+
 				for (DispatcherMap::iterator iter = dispatcherMap.begin(); iter != dispatcherMap.end(); ++iter) {
 					string exchange((*iter).first); // e.g., "OWConfig"
 
 					amqp_exchange_declare(conn.get(), 1, amqp_cstring_bytes(exchange.c_str()), amqp_cstring_bytes("fanout"), 0, 0, amqp_empty_table);
 					die_on_amqp_error(amqp_get_rpc_reply(conn.get()), "amqp_exchange_declare");
 
-					amqp_queue_declare_ok_t *r = amqp_queue_declare(conn.get(), 1, amqp_empty_bytes, 0, 0, 0, 1, amqp_empty_table);
-					die_on_amqp_error(amqp_get_rpc_reply(conn.get()), "amqp_queue_declare");
-
-					auto_amqp_bytes queuename(amqp_bytes_malloc_dup(r->queue));
-
-					amqp_queue_bind(conn.get(), 1, queuename.get(), amqp_cstring_bytes(exchange.c_str()), amqp_cstring_bytes(""/*bindingkey*/), amqp_empty_table);
+					amqp_queue_bind(conn.get(), 1, privateQueue.get(), amqp_cstring_bytes(exchange.c_str()), amqp_cstring_bytes(""/*bindingkey*/), amqp_empty_table);
 					die_on_amqp_error(amqp_get_rpc_reply(conn.get()), "amqp_queue_bind");
-
-					amqp_basic_consume(conn.get(), 1, queuename.get(), amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
-					die_on_amqp_error(amqp_get_rpc_reply(conn.get()), "amqp_basic_consume");
 				}
 
 				run(conn.get());
@@ -251,6 +252,7 @@ public:
 		return 0;
 	}
 
+private:
 	void run(amqp_connection_state_t conn)
 	{
 	  int received = 0;
